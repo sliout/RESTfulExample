88580294a32288929c82e7eaa106273d8c7fd43b
Serguei Lioutikov <Serguei.Lioutikov@gmail.com>
2016-05-15 22:02:22 +0100

    added src/main/java/sliout/test/Customer.java

---
+++ src/main/java/sliout/test/Customer.java
@@ -0,0 +1,27 @@
+package sliout.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Serguei on 5/15/2016.
+ */
+/**
+ * Root resource (exposed at "customer" path)
+ */
+@Path("customer")
+public class Customer {
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Customer";
+    }
+}
